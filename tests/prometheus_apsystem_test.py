d456c7cba91be3a7535335f4624178ccfc0486c9
arekp <arkadiusz.ptak@orange.com>
2021-07-12 15:31:24 +0200

    ok

---
+++ tests/prometheus_apsystem_test.py
@@ -0,0 +1,22 @@
+import pytest
+
+def test_energy():
+    """Sprawdzenie pobierania danych energii"""
+    assert True
+def test_power(): 
+    """Sprawdzenie pobierania danych energii"""
+    assert True
+def test_text(elt):
+    """Sprawdzenie pobierania danych energii"""
+    assert True
+def test_tablica():
+    """Sprawdzenie pobierania danych energii"""
+    assert True
+
+def test_basesite():    
+    """Sprawdzenie pobierania danych energii"""
+    assert True    
+
+def test_stronaglowna(registry):    
+    """Sprawdzenie pobierania danych energii"""
+    assert True
