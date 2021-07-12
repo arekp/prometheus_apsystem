1b5f46fa52f431304133754a65c5c284451134ea
arekp <arkadiusz.ptak@orange.com>
2021-07-12 09:21:04 +0200

    kolejne poprawki

--- README.md
+++ README.md
@@ -3,4 +3,186 @@
 Data transformation from [APsystem](https://apsystems.com/) inverters to [Prometheus](https://prometheus.io) format.
 Provides power, energy, voltage and temperature metrics.
 
-![Grafana](https://github.com/arekp/prometheus_apsystem/blob/master/images/grafana.png)
+![Grafana](https://raw.githubusercontent.com/arekp/prometheus_apsystem/master/images/grafana.png)
+
+## Getting Started
+
+These instructions will install and run `prometheus_APsystems` on your system.
+
+### Running with Docker
+
+prometheus_APsystems` is also available as a [Docker](http://docker.com) image
+on [Docker Hub](https://hub.docker.com/)
+:whale:.
+
+```shell
+docker run --rm -d --name prometheus_APsystems --env HOST=http://192.168.1.24 -p 5000:5000/tcp arekp/prometheus_APsystems:latest
+```
+
+You can also append extra flags when running with Docker. For example:
+
+```shell
+$ docker run --rm --name prometheus_APsystems -p 5000:5000/tcp --env HOST=http://192.168.1.24  arekp/prometheus_APsystems:latest 
+```
+#### prometheus.yml config
+
+Add this to your
+[Prometheus config](https://prometheus.io/docs/prometheus/latest/configuration/configuration)
+to start instrumenting APsystems and recording their metrics.
+
+```yaml
+global:
+  scrape_timeout: 2m
+
+scrape_configs:
+- job_name: 'APsystems'
+  metrics_path: /metrics
+  static_configs:
+  - targets:
+    - localhost:5000
+```
+
+Note if you're running `prometheus` under Docker, you must link the
+`prometheus` container to `prometheus_APsystems`. See the steps below for how
+this can be done.
+
+#### Trying it out
+
+An example
+[Prometheus config](https://prometheus.io/docs/prometheus/latest/configuration/configuration)
+has been provided at
+[example/prometheus.yml](https://github.com/).
+We'll start `prometheus` with this config.
+
+
+### Instrumenting APsystems with cURL
+
+Once `prometheus_APsystems` has been started, with either Docker 
+APsystems can be instrumented with [cURL](https://curl.haxx.se).
+
+```shell
+$ curl localhost:5000/metrics
+# HELP panele_lifetimegeneration Lifetime Generation
+# TYPE panele_lifetimegeneration gauge
+panele_lifetimegeneration 4335.2
+# HELP panele_generationcurrentday Generation Current of Day
+# TYPE panele_generationcurrentday gauge
+panele_generationcurrentday 20.56
+# HELP panele_LastSystemPower Last System Power
+# TYPE panele_LastSystemPower gauge
+panele_LastSystemPower 468.0
+# HELP panele_Napiecie Napiecie na panelu
+# TYPE panele_Napiecie gauge
+panele_Napiecie{falownik="801000026000",panel="1"} 237.0
+panele_Napiecie{falownik="801000026000",panel="2"} 237.0
+panele_Napiecie{falownik="801000026000",panel="3"} 237.0
+panele_Napiecie{falownik="801000026000",panel="4"} 237.0
+panele_Napiecie{falownik="801000074230",panel="1"} 235.0
+panele_Napiecie{falownik="801000074230",panel="2"} 235.0
+panele_Napiecie{falownik="801000074230",panel="3"} 235.0
+panele_Napiecie{falownik="801000074230",panel="4"} 235.0
+panele_Napiecie{falownik="801000030076",panel="1"} 234.0
+panele_Napiecie{falownik="801000030076",panel="2"} 234.0
+panele_Napiecie{falownik="801000030076",panel="3"} 234.0
+panele_Napiecie{falownik="801000030076",panel="4"} 234.0
+# HELP panele_Moc moc na panelu
+# TYPE panele_Moc gauge
+panele_Moc{falownik="801000026000",panel="1"} 32.0
+panele_Moc{falownik="801000026000",panel="2"} 41.0
+panele_Moc{falownik="801000026000",panel="3"} 0.0
+panele_Moc{falownik="801000026000",panel="4"} 45.0
+panele_Moc{falownik="801000074230",panel="1"} 36.0
+panele_Moc{falownik="801000074230",panel="2"} 43.0
+panele_Moc{falownik="801000074230",panel="3"} 46.0
+panele_Moc{falownik="801000074230",panel="4"} 46.0
+panele_Moc{falownik="801000030076",panel="1"} 43.0
+panele_Moc{falownik="801000030076",panel="2"} 43.0
+panele_Moc{falownik="801000030076",panel="3"} 47.0
+panele_Moc{falownik="801000030076",panel="4"} 46.0
+# HELP panele_Temp Temperatura na panelu
+# TYPE panele_Temp gauge
+panele_Temp{falownik="801000026000"} 39.0
+panele_Temp{falownik="801000074230"} 38.0
+panele_Temp{falownik="801000030076"} 42.0
+# HELP panele_Czestotliwosc Czestotliwosc na panelu
+# TYPE panele_Czestotliwosc gauge
+panele_Czestotliwosc{falownik="801000026000"} 50.0
+panele_Czestotliwosc{falownik="801000074230"} 50.0
+panele_Czestotliwosc{falownik="801000030076"} 50.0
+```
+
+You can also visit <http://localhost:5000> in your browser to see the same
+metrics.
+
+## Getting Started (Development)
+
+These instructions will get you a copy `prometheus_APsystems` up and running on
+your local machine for development and testing purposes.
+
+### Prerequisites
+
+* [Python](https://www.python.org)
+* [Docker](https://www.docker.com)
+* [Pytest](https://pytest.org)
+
+### Running Locally
+
+#### Python
+
+1. Ensure packages listed in
+   [requirements.txt](https://github.com/arekp/prometheus_apsystem/blob/master/requirements.txt)
+   are installed with `pip`
+
+   ```python
+   pip3 install -r requirements.txt
+   ```
+
+1. Run `prometheus_APsystems`
+
+   ```python
+   python3 app.py
+   ```
+
+#### Docker
+
+1. Building image
+
+   ```shell
+   docker build -t prometheus_APsystems:latest .
+   ```
+
+1. Running
+
+   ```shell
+   docker run --rm -d --name prometheus_APsystems -p 5000:5000/tcp --env HOST=http://192.168.1.24 prometheus_APsystems:latest
+   ```
+
+### Perform a APsystems
+
+```shell
+curl localhost:5000/metrics
+```
+
+Or visit <http://localhost:5000/metrics>
+
+### Running Unit Tests
+
+```shell
+pytest
+```
+
+
+
+## Authors
+
+* Arkadiusz Ptak
+
+## License
+
+This product is licensed under the Apache 2.0 license. See [LICENSE](LICENSE)
+file for details.
+
+## Acknowledgments
+
+* The Prometheus team <https://prometheus.io>
+* Testing in Python team <http://lists.idyll.org/listinfo/testing-in-python>
--- prometheus_apsystem.py
+++ prometheus_apsystem.py
@@ -1,25 +1,18 @@
 import re
 from datetime import datetime
 from os import getenv
-import time
 import requests
 from dotenv import load_dotenv
 from lxml import html
 from requests.exceptions import Timeout
-from prometheus_client import Counter, Gauge, CollectorRegistry, generate_latest
-
+from prometheus_client import Gauge
 
 load_dotenv()
 print(getenv('HOST'))
 now = datetime.now()  # current date and tim
 
-# url = 'http://192.168.1.24/index.php/realtimedata/old_energy_graph'
-# url = 'http://192.168.1.24/index.php/realtimedata/old_power_graph'
-# urlHtml = f"{getenv('HOST')}/index.php/realtimedata"
-# urlStart = f"{getenv('HOST')}/index.php/home"
-
-
 def energy():
+    """W trakcie przygotowywania ."""
     url = f"{getenv('HOST')}/index.php/realtimedata/old_energy_graph"
     dzisiaj = now.strftime("%m/%d")
     print("date and time:", dzisiaj)
@@ -33,6 +26,7 @@ def energy():
 
 
 def power():
+    """W trakcie przygotowywania ."""
     url = f"{getenv('HOST')}/index.php/realtimedata/old_power_graph"
     dzisiaj = now.strftime("%m/%d")
     print("date and time:", dzisiaj)
@@ -46,8 +40,9 @@ def power():
 
 
 def text(elt):
+    """Formatowanie danych z tabelki."""
     obiekt = elt.text_content().replace(u'\xa0', u' ')
-    a=""
+    a = ""
     if len(re.findall("\d+-\d", obiekt)) == 1:
         a = re.findall("\d+-\d+", obiekt)[0]
         return a
@@ -56,16 +51,16 @@ def text(elt):
         a = re.findall("\d+", obiekt)[0]
     if obiekt.find("V") != -1:
         a = re.findall("\d+", obiekt)[0]
-        obiekt = (f"napiecie:{a}")
+        # obiekt = (f"napiecie:{a}")
     if (obiekt.find("C") != -1):
         a = re.findall("\d+", obiekt)[0]
-        obiekt = (f"temperatura:{a}")
+        # obiekt = (f"temperatura:{a}")
     if (obiekt.find("Hz") != -1):
         a = re.findall("\d+", obiekt)[0]
         obiekt = (f"czestotliwosc:{a}")
     if len(re.findall("\d+-\d+-\d+", obiekt)) == 1:
         a = obiekt.replace('\n', '')
-        obiekt = (f"data:{a}")
+        # obiekt = (f"data:{a}")
     if len(re.findall("-.", obiekt)) == 1:
         return 0
 
@@ -73,79 +68,89 @@ def text(elt):
 
 
 def tablica():
+    """Pobieranie danych z tablicy."""
     urlHtml = f"{getenv('HOST')}/index.php/realtimedata"
-    tabl=[]
+    tabl = []
     try:
         x = requests.get(urlHtml, timeout=3)
     except Timeout:
         print('The request timed out')
+        return tabl
+    except requests.exceptions.MissingSchema:
+        print('bład połaczenia')        
+        return tabl
     else:
         print(f'mamy strone tablica')
     root = html.fromstring(x.content)
-    lista=['falowing-panel','moc','czestotliwosc','napiecie','temperatura','data']
-    listb=['falowing-panel','moc','napiecie']
+    lista = ['falowing-panel', 'moc', 'czestotliwosc', 'napiecie', 'temperatura', 'data']
+    listb = ['falowing-panel', 'moc', 'napiecie']
     for table in root.xpath('//table/tbody'):
-        i=0
+        i = 0
         for tr in table.xpath('//tr[count(*) > 2]'):
             if (len(tr) == 6):
                 dane = list(text(td) for td in tr.xpath('td'))
                 if len(dane) == 6:
-                    tabl.append(dict(zip(lista,dane)))
+                    tabl.append(dict(zip(lista, dane)))
             if (len(tr) == 3):
                 dane = list(text(td) for td in tr.xpath('td'))
                 if len(dane) == 3:
-                    tabl.append(dict(zip(listb,dane)))
+                    tabl.append(dict(zip(listb, dane)))
     return tabl
-    
+
+
 def basesite():
     urlStart = f"{getenv('HOST')}/index.php/home"
+    a,b,d=0,0,0
     try:
         x = requests.get(urlStart, timeout=3)
     except Timeout:
         print('The request timed out')
+        return a,b,d
+    except requests.exceptions.MissingSchema:
+        print('bład połaczenia')
+        return a,b,d
     else:
         print(f'mamy basesite')
         root = html.fromstring(x.content)
         LifetimeGeneration = root.xpath(
-            '//table[@class="table table-condensed table-striped table-bordered"]/tr[2]/td[1]/text()')[0]            
+            '//table[@class="table table-condensed table-striped table-bordered"]/tr[2]/td[1]/text()')[0]
         LastSystemPower = \
             root.xpath('//table[@class="table table-condensed table-striped table-bordered"]/tr[3]/td[1]/text()')[0]
         GenerationCurrentDay = root.xpath(
             '//table[@class="table table-condensed table-striped table-bordered"]/tr[4]/td[1]/text()')[0]
         a = re.findall("\d+", LastSystemPower)[0]
         b = re.findall("^(\d+(?:[\.\,]\d{1,2})?)", GenerationCurrentDay)[0]
-        d= re.findall("^(\d+(?:[\.\,]\d{1,2})?)", LifetimeGeneration)[0]
-        return a,b,d
+        d = re.findall("^(\d+(?:[\.\,]\d{1,2})?)", LifetimeGeneration)[0]
+        return a, b, d
+
 
 def stronaglowna(registry):
-    a,b,d = basesite()
+    a, b, d = basesite()
     e = Gauge(name="panele_lifetimegeneration", documentation="Lifetime Generation", registry=registry)
     e.set(d)
     c = Gauge(name="panele_generationcurrentday", documentation="Generation Current of Day", registry=registry)
     c.set(b)
     g = Gauge('panele_LastSystemPower', 'Last System Power', registry=registry)
     g.set(a)  # Set to a given value
-    nap = Gauge('panele_Napiecie', 'Napiecie na panelu',['falownik', 'panel'], registry=registry)
-    moc = Gauge('panele_Moc', 'moc na panelu',['falownik', 'panel'], registry=registry)
-    temp = Gauge('panele_Temp', 'Temperatura na panelu',['falownik'], registry=registry)
-    czest = Gauge('panele_Czestotliwosc', 'Czestotliwosc na panelu',['falownik'], registry=registry)
+    nap = Gauge('panele_Napiecie', 'Napiecie na panelu', ['falownik', 'panel'], registry=registry)
+    moc = Gauge('panele_Moc', 'moc na panelu', ['falownik', 'panel'], registry=registry)
+    temp = Gauge('panele_Temp', 'Temperatura na panelu', ['falownik'], registry=registry)
+    czest = Gauge('panele_Czestotliwosc', 'Czestotliwosc na panelu', ['falownik'], registry=registry)
 
     for wpis in tablica():
-        if wpis['falowing-panel']!='' and len(wpis)==6:
+        if wpis['falowing-panel'] != '' and len(wpis) == 6:
             fal = wpis['falowing-panel'].split("-")[0]
             pan = wpis['falowing-panel'].split("-")[1]
-            nap.labels(falownik=fal,panel=pan).set(wpis['napiecie'])
-            moc.labels(falownik=fal,panel=pan).set(wpis['moc'])
+            nap.labels(falownik=fal, panel=pan).set(wpis['napiecie'])
+            moc.labels(falownik=fal, panel=pan).set(wpis['moc'])
             temp.labels(falownik=fal).set(wpis['temperatura'])
             czest.labels(falownik=fal).set(wpis['czestotliwosc'])
-        if wpis['falowing-panel']!='' and len(wpis)==3:
+        if wpis['falowing-panel'] != '' and len(wpis) == 3:
             fal = wpis['falowing-panel'].split("-")[0]
             pan = wpis['falowing-panel'].split("-")[1]
-            nap.labels(falownik=fal,panel=pan).set(wpis['napiecie'])
-            moc.labels(falownik=fal,panel=pan).set(wpis['moc'])
+            nap.labels(falownik=fal, panel=pan).set(wpis['napiecie'])
+            moc.labels(falownik=fal, panel=pan).set(wpis['moc'])
     return registry
 
 
-
-
-
+# basesite()
